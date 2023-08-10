f0ec27f0d6333a5085db7ee81d0e7b632787c665
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-10 00:06:54 -0500

    fixed typo in products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -129,7 +129,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No tag found with this id!'});
       return;
     }
-    res.status(404).json(productData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
